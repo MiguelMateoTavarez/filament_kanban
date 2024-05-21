a76506d42ab98390387b773cb434b825357ff7b5
Miguel Mateo <miguelmateot94@gmail.com>
2024-05-21 13:36:02 -0400

    Configure tasks and column relationship

--- app/Models/Column.php
+++ app/Models/Column.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+use AnourValar\EloquentSerialize\Package;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Column extends Model
 {
     use HasFactory;
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
 }
